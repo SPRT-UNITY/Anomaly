c0b55453edca8ad1dbcd545ca35a22ba4d008f93
oeto2 <oeto2@naver.com>
2024-02-23 16:10:26 +0900

    Feat 타이틀 에셋 추가

---
+++ Anomaly/Assets/99.LSM Folder/Scripts/Base/SingletoneBase.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingletoneBase<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T _instance;
+
+    //Property
+    public static T Instance
+    {
+        get
+        {
+            if(_instance == null)
+            {
+                //Create Object
+                string typeName = typeof(T).Name;
+                GameObject obj = new GameObject(typeName);
+
+                _instance = obj.AddComponent<T>();
+                DontDestroyOnLoad(obj);
+            }
+            return _instance;
+        }
+    }
+
+    public virtual void Init()
+    {
+        Debug.Log(transform.name = "is Init");
+    }
+}
---
+++ Anomaly/Assets/99.LSM Folder/Scripts/Managers/ResourceManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class ResourceManager : SingletoneBase<ResourceManager>
+{
+    //Load Resources
+    public T Load<T>(string path) where T : Object
+    {
+        return Resources.Load<T>(path);
+    }
+
+    //Instantiate Object
+    public GameObject Instantiate(string path, Transform parent = null)
+    {
+        GameObject prefab = Load<GameObject>($"Prfabs/{path}");
+        if(prefab != null)
+        {
+            Debug.Log($"Failed to load prefab : {path}");
+            return null;
+        }
+
+        return Instantiate(prefab, parent);
+    }
+
+    public GameObject Instantiate(GameObject prefab, Transform parent = null)
+    {
+        GameObject obj = Object.Instantiate(prefab, parent);
+        obj.name = prefab.name;
+        return obj;
+    }
+
+    public void Destroy(GameObject obj)
+    {
+        if (obj = null)
+            return;
+
+        Object.Destroy(obj);
+    }
+}
