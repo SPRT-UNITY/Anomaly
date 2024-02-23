a4e425fb9317b972ebe115e93bb7c1a3990ab986
oeto2 <oeto2@naver.com>
2024-02-23 23:28:53 +0900

    Feat 카메라 전환 기능 추가

---
+++ Anomaly/Assets/99.LSM Folder/Scripts/Camera/CameraController.cs
@@ -0,0 +1,59 @@
+using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private CinemachineVirtualCamera[] ViewCam;
+    [SerializeField] private int curCamNumber = 0;
+
+    private int viewCamCount;
+
+    private void Start()
+    {
+        viewCamCount = ViewCam.Length -1;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            ShowNextCam();
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            ShowBeforeCam();
+        }
+    }
+
+    public void ShowNextCam()
+    {
+        if (curCamNumber < viewCamCount)
+        {
+            ViewCam[curCamNumber++].Priority = 0;
+            ViewCam[curCamNumber].Priority = 10;
+        }
+        else
+        {
+            curCamNumber = 0;
+            ViewCam[curCamNumber].Priority = 10;
+        }
+    }
+
+    public void ShowBeforeCam()
+    {
+        if (curCamNumber > 0)
+        {
+            ViewCam[curCamNumber--].Priority = 0;
+            ViewCam[curCamNumber].Priority = 10;
+        }
+        else
+        {
+            ViewCam[curCamNumber].Priority = 0;
+            curCamNumber = viewCamCount;
+            ViewCam[curCamNumber].Priority = 10;
+        }
+    }
+}
