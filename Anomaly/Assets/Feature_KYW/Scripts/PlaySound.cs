3c1b4849042250b1a6c4fea5775b30307f2f30b3
purangi <kaya0728@naver.com>
2024-02-29 15:08:54 +0900

    Feat: 볼륨 컨트롤, 피아노 브금 구현

---
+++ Anomaly/Assets/Feature_KYW/Scripts/PlaySound.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySound : MonoBehaviour
+{
+    [SerializeField] bool isBGM;
+
+    public AudioClip clip;
+    
+    private void Start()
+    {
+        if(isBGM) PlayBGM();
+    }
+
+    public void PlayBGM()
+    {
+        SoundManager.instance.PlayBGM(clip);
+    }
+
+    public void PlaySFX()
+    {
+        SoundManager.instance.PlaySFX(clip);
+    }
+}
---
+++ Anomaly/Assets/Feature_KYW/Scripts/VolumeController.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class VolumeController : MonoBehaviour
+{
+    [SerializeField] AudioMixer audioMixer;
+
+    [SerializeField] Slider bgmSlider;
+    [SerializeField] Slider sfxSlider;
+
+    private void Start()
+    {
+        SetVolume();
+    }
+
+    public void VolumeControl(string _type)
+    {
+        Slider slider;
+
+        switch (_type)
+        {
+            case "BGM":
+                slider = bgmSlider;
+                break;
+            case "SFX":
+                slider = sfxSlider;
+                break;
+            default:
+                slider = bgmSlider;
+                break;
+        }
+
+        float volume = slider.value;
+
+        if (volume == -40f) audioMixer.SetFloat(_type, -80);
+        else audioMixer.SetFloat(_type, volume);
+    }
+
+    void SetVolume()
+    {
+        audioMixer.GetFloat("BGM", out float bgmVolume);
+        bgmSlider.value = bgmVolume;
+        audioMixer.GetFloat("SFX", out float sfxVolume);
+        sfxSlider.value = sfxVolume;
+    }
+}
