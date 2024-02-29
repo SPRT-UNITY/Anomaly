689184538166318065047fc2ce6f87673173ab6b
oeto2 <oeto2@naver.com>
2024-02-29 13:53:49 +0900

    Revert "Feat : 수정사항 적용"
    This reverts commit 1d97e3a19f89c57d93fe8dae0f0177f0a45fca25.

--- Anomaly/Assets/99.LSM Folder/Scripts/Title/TitleGameStartCanvas.cs
+++ Anomaly/Assets/99.LSM Folder/Scripts/Title/TitleGameStartCanvas.cs
@@ -11,27 +11,6 @@ public class TitleGameStartCanvas : UIBase
     private void Awake()
     {
         ExitButton.onClick.AddListener(() => CloseUI());
-<<<<<<< Updated upstream
         StartHouseButton.onClick.AddListener(() => LoadSceneController.LoadScene("LSM_Scene2"));
-=======
-        InitButtons();
-    }
-
-    private void InitButtons()
-    {
-        //Button Object Settings
-        foreach(var button in StartButtons)
-        {
-            OnPointerEnter_Button onPointerEnter = button.AddComponent<OnPointerEnter_Button>();
-            onPointerEnter.OnMouseImage = ResourceManager.Instance.Instantiate("Title/OnMouse_Image", button.transform);
-        }
-
-        //Caching Buttons
-        Button backButton = StartButtons[(int)TitleStartButtonType.Back];
-
-        //Button OnClick AddListeners
-        StartButtons[(int)TitleStartButtonType.House].onClick.AddListener(() => LoadSceneController.LoadScene("MainScene"));
-        backButton.onClick.AddListener(() => backButton.GetComponent<OnPointerEnter_Button>().OnMouseImage_ActiveFalse());
->>>>>>> Stashed changes
     }
 }
